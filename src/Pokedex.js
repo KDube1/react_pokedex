c63dc0b53b8acb50c313c3cc9fd66678ea232500
KDube1 <32679937+KDube1@users.noreply.github.com>
2020-01-19 00:43:11 -0500

    Initial commit

---
+++ src/Pokedex.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from "react";
+import { PokemonType } from "./PokemonType";
+
+export function Pokedex() {
+  const [selectedPokemon, setSelectedPokemon] = useState(undefined);
+  const searchBox = useRef(null);
+  async function getPokemonInfo(name) {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+    const response = await fetch(URL);
+    if (!response.ok) {
+      alert("Pokemon does not exist");
+      return;
+    }
+    const data = await response.json();
+    setSelectedPokemon(data);
+  }
+
+  return (
+    <div>
+      {selectedPokemon && (
+        <div>
+          <h4>{selectedPokemon.species.name}</h4>
+
+          <PokemonType type={selectedPokemon.types[0].type.name} />
+          {selectedPokemon.types.length > 1 && (
+            <PokemonType type={selectedPokemon.types[1].type.name} />
+          )}
+          <img src={selectedPokemon.sprites.front_default} alt="sprite" />
+        </div>
+      )}
+
+      <input ref={searchBox} />
+
+      <button onClick={() => getPokemonInfo(searchBox.current.value)}>
+        Search
+      </button>
+      <ul />
+    </div>
+  );
+}
